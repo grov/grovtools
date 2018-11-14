f36837f772bc6f6c6cd6f4193d8d804633df58bd
Johnny John Johnnson <yoann.audon@gmail.com>
2018-11-14 15:42:34 +0100

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while read p; do
-  echo "$p" | tr '\n' '\t'  && ping -c 1 "$p" | head -1 | sed 's/.*(\(.*\)).*(\(.*\)).*/\1/'
+  echo "$p" | tr '\n' '\t' && ping -c 1 "$p" | head -1 | sed 's/.*(\(.*\)).*(\(.*\)).*/\1/'
 done <ping.txt
