07a33b0a4bda197dc913ce1ffed3c2cf04271138
Johnny John Johnnson <yoann.audon@gmail.com>
2022-08-10 12:15:21 +0200

    Create csv2email2hibp.sh

---
+++ csv2email2hibp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Premiere partie : Recuperation des emails
+echo "Récupération des emails dans le fichier csv :"
+#Nom du fichier source :
+echo "Nom du fichier source :"
+read source
+
+echo "Le nom du fichier d'output est : email.txt"
+
+grep -oe "[a-zA-Z0-9._]\+@[a-zA-Z]\+.[a-zA-Z]\+" $source > email.txt
+
+sleep 2
+
+# Deuxième partie : récupération des dataleak avec l'API Have i Been Pwned
+echo "Analyse des dataleaks avec Have I Been Pwned :"
+
+# Lancement du script HIPB2.sh
+bash HIBP.sh > result.txt
+
+echo "Les résultats sont dans le fichier result.txt"
