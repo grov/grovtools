57f6f88ac4b5a157e4170fe693a9282574df5f68
Johnny John Johnnson <yoann.audon@gmail.com>
2022-08-10 15:47:14 +0200

    Update csv2email2hibp.sh

--- csv2email2hibp.sh
+++ csv2email2hibp.sh
@@ -2,16 +2,17 @@
 
 # Copie des anciens fichiers dans le répertoire OLD"
 echo "*** Copie des anciens fichiers dans le répertoire OLD ***"
+if [ -f email.txt -a -f email_unique.txt -a -f result.txt ]; then
 mv email.txt OLD/email.txt.$(date +%F_%R)
 mv email_unique.txt OLD/email_unique.txt.$(date +%F_%R)
 mv result.txt OLD/result.txt.$(date +%F_%R)
-
+fi
 
 # Première partie : Récupération des emails
 echo "*** Récupération des emails dans l'extract :*** "
 
-#Nom du fichier source :
-echo "*** Nom du fichier source : ***"
+#Nom du fichier CSV source :
+echo "*** Quel est le nom du fichier CSV source ? ***"
 read source
 
 echo "*** Le nom du fichier d'output est : email.txt ***"
@@ -20,7 +21,7 @@ grep -oe "[a-zA-Z0-9._]\+@[a-zA-Z]\+.[a-zA-Z]\+" $source > email.txt
 
 echo "*** Suppression des doublons ***"
 sort email.txt | uniq > email_unique.txt
-echo " *** Les emails uniques se trouvent dans le fichier email_unique.txt ***"
+echo "*** Les emails uniques se trouvent dans le fichier email_unique.txt ***"
 
 sleep 2
 
